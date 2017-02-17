377e93613e6f6fc493bd79aeb23d5c66c7740f40
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-02-17 17:02:02 +0700

    learn spring actuator

---
+++ spring-actuator/src/main/java/com/github/ipan97/spring/actuator/SpringActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.github.ipan97.spring.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorApplication.class, args);
+	}
+}
